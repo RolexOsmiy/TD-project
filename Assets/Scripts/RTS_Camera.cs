e4bafa787c74631f84523b62de9c746fd47f4057
RolexOsmiy <wow.rolex@yandex.by>
2017-04-15 13:01:45 +0300

    update

---
+++ Assets/Scripts/RTS_Camera.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RTS_Camera : MonoBehaviour {
+
+	public float scrollZone = 30;
+	public float scrollSpeed = 5;
+
+	public float xMax = 8;
+	public float xMin = 0;
+	public float yMax = 8;
+	public float yMin = 0;
+	public float zMax = 8;
+	public float zMin = 0;
+
+	private Vector3 desiredPosition;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	private void Update () 
+	{
+		float x = 0, y = 10, z = 0;
+		float speed = scrollZone * Time.deltaTime;
+		if (Input.mousePosition.x < 30) 
+		{
+			x -= speed;
+		}
+		else if (Input.mousePosition.x > Screen.width - scrollZone)
+		{
+			x += speed;
+		}
+		if (Input.mousePosition.y < 30) 
+		{
+			z -= speed;
+		}
+		else if (Input.mousePosition.y > Screen.height - scrollZone)
+		{
+			z += speed;
+		}
+		y += Input.GetAxis ("Mouse ScrollWheel");
+		Vector3 move = new Vector3 (x, y, z) + desiredPosition;
+		move.x = Mathf.Clamp (move.x,xMin,xMax);
+		move.y = Mathf.Clamp (move.y,yMin,yMax);
+		move.z = Mathf.Clamp (move.z,zMin,zMax);
+		desiredPosition = move;
+		transform.position = Vector3.Lerp (transform.position, desiredPosition, 0.2f);
+	}
+}
---
+++ Assets/Scripts/Skill_Control.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Skill_Control : MonoBehaviour {
+
+	public static int skillpoints = 5;
+
+	public static int skill1 = 0;
+	public static int skill2 = 0;
+	public static int skill3 = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		if ((gameObject.name == "Tier1_Spell2") && (skillpoints > 0)) 
+		{
+			GetComponent<SpriteRenderer> ().color = new Color (0, 0, 0);
+		}
+		if ((gameObject.name == "Tier1_Spell3") && (skillpoints > 0)) 
+		{
+			GetComponent<SpriteRenderer> ().color = new Color (0, 0, 0);
+		}
+		if ((gameObject.name == "Tier1_Spell3") && (skillpoints > 0)) 
+		{
+			GetComponent<SpriteRenderer> ().color = new Color (0, 0, 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
